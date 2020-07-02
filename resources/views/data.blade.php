589d5b67a61b3caa69f0aa12bda9b3581484b717
randhikanovan <randhika.novan@ti.ukdw.ac.id>
2020-07-02 12:21:53 +0700

    update asset push

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -442,8 +442,8 @@
 @endsection
 
 @push('script')
-    <script src="../../plugins/datatables/jquery.dataTables.js"></script>
-    <script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+    <script src="{{asset('adminlte/plugins/datatables/jquery.dataTables.js')}}"></script>
+    <script src="{{asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
     <script>
     $(function () {
         $("#example1").DataTable();
